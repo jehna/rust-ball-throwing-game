4ebf6a8eef24383e8a1baecdbf82d7c845d671f8
Jesse Luoto <jesse.luoto@gmail.com>
2022-10-28 22:14:43 +0300

    Add new players on connection

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,25 +1,40 @@
 use futures_util::{SinkExt, StreamExt};
-use rust_ball_throwing_multipleyer_game::data_channel::ServerMessage;
+use rust_ball_throwing_multipleyer_game::data_channel::{ServerMessage, UserId};
 use std::io::Error;
+use std::sync::{Arc, Mutex};
 use tokio::net::{TcpListener, TcpStream};
+use tokio::sync::mpsc::{unbounded_channel, UnboundedSender};
 use tokio_tungstenite::{accept_async, tungstenite::Message};
+
+type AllConnections = Arc<Mutex<Vec<UnboundedSender<Message>>>>;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     let listener = TcpListener::bind("127.0.0.1:8080").await?;
+    let all_connections = AllConnections::new(Mutex::new(Vec::new()));
 
+    let mut id: UserId = 0;
     while let Ok((stream, _)) = listener.accept().await {
-        tokio::spawn(accept_connection(stream));
+        let curr_id = id;
+        id += 1;
+        tokio::spawn(accept_connection(stream, curr_id, all_connections.clone()));
     }
 
     Ok(())
 }
 
-async fn accept_connection(stream: TcpStream) -> Result<(), Error> {
+async fn accept_connection(
+    stream: TcpStream,
+    user_id: UserId,
+    all_connections: AllConnections,
+) -> Result<(), Error> {
     let addr = stream
         .peer_addr()
         .expect("connected streams should have a peer address");
     println!("Peer address: {}", addr);
+
+    let (sender, mut receiver) = unbounded_channel::<Message>();
+    all_connections.lock().unwrap().push(sender.clone());
 
     let ws_stream = accept_async(stream).await.expect("Failed to accept");
 
@@ -27,35 +42,33 @@ async fn accept_connection(stream: TcpStream) -> Result<(), Error> {
 
     let (mut write, mut read) = ws_stream.split();
 
-    // Sleep for 2 seconds, then send a message.
-    tokio::time::sleep(tokio::time::Duration::from_secs(2)).await;
+    tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
 
-    write.send(Message::Ping(vec![1])).await.unwrap();
-
-    let messages = vec![
-        &ServerMessage::NewPlayerJoined(123),
-        &ServerMessage::SetLocalUserId(123),
-    ];
-
-    for message in messages.iter() {
-        let encoded = bincode::serialize(message).unwrap();
-        write.send(Message::Binary(encoded)).await.unwrap();
+    // Broadcast new player
+    for connection in all_connections.lock().unwrap().iter() {
+        let message = bincode::serialize(&ServerMessage::NewPlayerJoined(user_id)).unwrap();
+        connection.send(Message::binary(message)).unwrap();
     }
 
-    tokio::time::sleep(tokio::time::Duration::from_secs(10)).await;
-
-    write
-        .send(Message::Binary(
-            bincode::serialize(&ServerMessage::NewPlayerJoined(111)).unwrap(),
+    // Set local player
+    sender
+        .send(Message::binary(
+            bincode::serialize(&ServerMessage::SetLocalUserId(user_id)).unwrap(),
         ))
-        .await
         .unwrap();
 
-    // Keep socket open until the client closes it.
-    while let Some(message) = read.next().await {
-        let message = message.unwrap();
-        println!("Received a message from {}: {}", addr, message);
-    }
+    tokio::spawn(async move {
+        while let Some(message) = read.next().await {
+            let message = message.unwrap();
+            println!("Received a message from {}: {}", addr, message);
+        }
+    });
+
+    tokio::spawn(async move {
+        while let Some(message) = receiver.recv().await {
+            write.send(message).await.unwrap();
+        }
+    });
 
     Ok(())
 }
